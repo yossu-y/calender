f62f115b4ef3e47a37ba8e525c6aa3a60c3f741f
yossu-y <yangyuub@gmail.com>
2022-07-18 04:15:54 +0000

    [add]header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_ssanitizer.permit(:sign_up, key: [:name, :nickname, :email])
+  end
+
 end
