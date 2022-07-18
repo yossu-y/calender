5e26aeba591245bfd870d0f03a0cd5d18d4d6bd6
yossu-y <yangyuub@gmail.com>
2022-07-18 02:53:53 +0000

    [add]post.user-model

---
+++ db/migrate/20220718025213_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
