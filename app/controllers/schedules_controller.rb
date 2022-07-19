6a8025ef61c9c639f6f0dbe792dbe2a15413227f
yossu-y <yangyuub@gmail.com>
2022-07-19 11:40:59 +0000

    [update]calender

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,28 @@
 class SchedulesController < ApplicationController
+
   def index
-    # @schedule = Schedule.all
+    @schedule = Schedule.all
   end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    @schedule.user_id = current_user.id
+    if @schedule.save
+      redirect_to schedules_path, noticd: "スケジュールを登録しました！"
+    else
+      @schedule = Schedule.all
+      render "index"
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:title, :body, :start_time)
+  end
+
 end
